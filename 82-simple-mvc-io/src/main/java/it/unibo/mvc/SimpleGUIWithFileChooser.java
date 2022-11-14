d61781ddf876f1e8478ec267a61da759312c3bdb
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-14 19:03:26 +0100

    First implementation, browse button listener to be fixed

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,6 +1,20 @@
 package it.unibo.mvc;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
 /**
@@ -9,6 +23,85 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUIWithFileChooser {
 
+    private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame();
 
+    public SimpleGUIWithFileChooser() {
+        // Setting the app canvas that will contain all the components.
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        // Creating and adding components to the canvas.
+        final JTextArea textArea = new JTextArea();
+        final JButton saveButton = new JButton("Save");
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(saveButton, BorderLayout.SOUTH);
+
+        // Part 3
+        final JPanel fileChooserPanel = new JPanel();
+        final JTextField textField = new JTextField();
+        final JButton browseButton = new JButton("Browse...");
+        textField.setEditable(false);
+        textField.setText(new Controller().getCurrentFile().getName());
+        fileChooserPanel.setLayout(new BorderLayout());
+        fileChooserPanel.add(textField, BorderLayout.CENTER);
+        fileChooserPanel.add(browseButton, BorderLayout.EAST);
+        canvas.add(fileChooserPanel, BorderLayout.NORTH);
+
+        // Setting the frame.
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Even handlers.
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                final var controller = new Controller();
+                try {
+                    controller.saveToFile(textArea.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        browseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                // Creating and displaying the file chooser.
+                final JFileChooser chooser = new JFileChooser();
+                final var choice = chooser.showSaveDialog(browseButton);
+
+                //TODO make it into a switch case
+                if (choice == JFileChooser.APPROVE_OPTION) {
+                    final File selectedFile = chooser.getSelectedFile();
+                    final var controller = new Controller();
+                    controller.setCurrentFile(selectedFile.getName());  //TODO change controller to get File as input maybe?
+                    textField.setText(new Controller().getCurrentFile().getName());
+                    textField.repaint();    //TODO not working
+                } else if (choice == JFileChooser.CANCEL_OPTION) {
+                    
+                } else {
+                    //error
+                }
+            }
+        });
+    }
+
+    /**
+     * Displays the graphical interface.
+     */
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int height = (int) screen.getHeight();
+        final int width = (int) screen.getWidth();
+        frame.setSize(width / PROPORTION, height / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUIWithFileChooser().display();
+    }
 }
