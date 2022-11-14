6efc05f024243a488edddbbf859881cf4f0aea28
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-14 14:14:47 +0100

    Implemented SimpleGUI

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,53 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI() {
+        // Setting the app canvas that will contain all the components.
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        // Creating and adding components to the canvas.
+        final JTextArea textArea = new JTextArea();
+        final JButton saveButton = new JButton("Save");
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(saveButton, BorderLayout.SOUTH);
+
+        // Setting the frame.
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Even handlers.
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                final var controller = new Controller();
+                try {
+                    controller.saveToFile(textArea.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Displays the graphical interface.
+     */
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int height = (int) screen.getHeight();
+        final int width = (int) screen.getWidth();
+        frame.setSize(width / PROPORTION, height / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
