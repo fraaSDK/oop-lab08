4d28afb587f7485420902df0895531348a588734
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-16 00:32:15 +0100

    Implemented the controller and GUI

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,41 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String currentString;
+    private List<String> stringHistory = new ArrayList<>();
+
+    @Override
+    public void setNextString(final String string) {
+        this.currentString = Objects.requireNonNull(string);
+    }
+
+    @Override
+    public String getNextString() {
+        return Objects.requireNonNull(this.currentString);
+    }
+
+    @Override
+    public List<String> getPrintedStringHistory() {
+        return new ArrayList<>(stringHistory);
+    }
+
+    @Override
+    public void printCurrentString() throws IllegalStateException {
+        if (Objects.nonNull(this.currentString)) {
+            System.out.println(this.currentString);
+            stringHistory.add(this.currentString);
+        } else {
+            throw new IllegalStateException();
+        }
+    }
 
 }
--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,66 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 2;
     private final JFrame frame = new JFrame();
+    private final Controller controller;
+
+    public SimpleGUI() {
+        this.controller = new SimpleController();
+
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        final JTextField textField = new JTextField();
+        final JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        final JButton printButton = new JButton("Print");
+        final JButton showHistoryButton = new JButton("Show history");
+
+        canvas.add(textField, BorderLayout.NORTH);
+        canvas.add(textArea, BorderLayout.CENTER);
+
+        final JPanel buttons = new JPanel();
+        buttons.add(printButton);
+        buttons.add(showHistoryButton);
+        canvas.add(buttons, BorderLayout.SOUTH);
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Event handlers
+        printButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                controller.setNextString(textField.getText());
+                controller.printCurrentString();
+            }
+        });
+
+        showHistoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                final StringBuilder result = new StringBuilder();
+                List<String> history = controller.getPrintedStringHistory();
+                for (final String string : history) {
+                    result.append(string + "\n");
+                }
+                textArea.setText(result.toString());
+            }
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int height = (int) screen.getHeight();
+        final int width = (int) screen.getWidth();
+        frame.setSize(height / PROPORTION, width / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
