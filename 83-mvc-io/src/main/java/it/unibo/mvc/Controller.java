34fec9beb0ed228ddf27c51232a666df677c5653
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-15 18:26:48 +0100

    Controller interface implementation

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,33 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    /**
+     * Sets a string to be printed next.
+     * @param string to print.
+     */
+    void setNextString(final String string);
+
+    /**
+     * @return the next string to be printed.
+     */
+    String getNextString();
+
+    /**
+     * @return a {@code List<String>} containing
+     * all the previously printed strings.
+     */
+    List<String> getPrintedStringHistory();
+
+    /**
+     * Prints the current string.
+     * @throws IllegalStateException if the current string is unset.
+     */
+    void printCurrentString() throws IllegalStateException;
 
 }
