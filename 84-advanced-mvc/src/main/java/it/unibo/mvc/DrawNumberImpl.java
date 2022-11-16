b58a82909ecc4aead49559a55c06cc2cec814bc5
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-16 21:07:23 +0100

    Edited the constructor to take a Configuration type as arg

--- 84-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberImpl.java
+++ 84-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberImpl.java
@@ -17,10 +17,13 @@ public final class DrawNumberImpl implements DrawNumber {
     /**
      * @throws IllegalStateException if the configuration is not consistent
      */
-    public DrawNumberImpl(final int min, final int max, final int attempts) {
-        this.min = min;
-        this.max = max;
-        this.attempts = attempts;
+    public DrawNumberImpl(final Configuration config) {
+        if (!config.isConsistent()) {
+            throw new IllegalStateException("Invalid configuration");
+        }
+        this.min = config.getMin();
+        this.max = config.getMax();
+        this.attempts = config.getAttempts();
         this.reset();
     }
 
